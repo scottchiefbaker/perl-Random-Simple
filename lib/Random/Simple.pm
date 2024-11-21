dfda3da47ff5e274e0a97f06c8a5d95ff1fcccf7
Scott Baker <scott@perturb.org>
2024-11-21 14:29:55 -0800

    Spell out pseudorandom number generator

--- lib/Random/Simple.pm
+++ lib/Random/Simple.pm
@@ -193,10 +193,10 @@ think about it.
 
 =head2 Methodology
 
-Perl's internal C<rand()> function uses C<drand48()> which is an older PRNG,
-and may have limitations. `Random::Simple` uses PCG which is: modern, simple,
-well vetted, and fast. `Random::Simple` will upgrade/override the `rand()`
-function to use a better PRNG.
+Perl's internal C<rand()> function uses C<drand48()> which is an older
+pseudorandom number generator and may have limitations. `Random::Simple` uses
+PCG which is: modern, simple, well vetted, and fast. `Random::Simple` will
+upgrade/override the `rand()` function to use a better PRNG.
 
 C<Random::Simple> is automatically seeded with entropy directly
 from your OS. On Linux this is C</dev/urandom> and on Windows it uses
