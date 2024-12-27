9bbe1f1f50c2781fd88e0e60c678cc65b70ded8d
Scott Baker <scott@perturb.org>
2024-12-27 13:06:37 -0800

    Add a comment for reference to the version numbering docs

--- lib/Random/Simple.pm
+++ lib/Random/Simple.pm
@@ -5,6 +5,7 @@ use warnings;
 use Time::HiRes;
 use Carp qw(croak);
 
+# https://pause.perl.org/pause/query?ACTION=pause_operating_model#3_5_factors_considering_in_the_indexing_phase
 our $VERSION = '0.16';
 our $debug   = 0;
 
