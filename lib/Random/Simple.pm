83c17790d7dc49ec26b7b00bf5fe8d1bcdf5b518
Scott Baker <scott@perturb.org>
2025-10-25 16:23:35 -0700

    Wording

--- lib/Random/Simple.pm
+++ lib/Random/Simple.pm
@@ -288,10 +288,10 @@ sub srand {
 		$seed = int(rand() * 4294967295); # Random 32bit int
 	}
 
-	# See has to be an integer
+	# Seed has to be an integer
 	$seed = int($seed);
 
-	# Convert the one 32bit seed into 2x 64bit seeds
+	# Convert one 32bit seed into 2x 64bit seeds
 	my $seed1 = _hash_mur3($seed);  # C API
 	my $seed2 = _hash_mur3($seed1); # C API
 
